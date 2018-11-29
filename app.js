7e7a5fb437220b524d8eec0729ee487eb43881c2
Marco <marco.moreno@rokode.com>
2018-11-29 17:21:38 -0600

    Se convierte en API, procesando el enpoti GET

--- app.js
+++ app.js
@@ -1,43 +1,54 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var bodyParser = require('body-parser');
+var app;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var blockchainRouter = require('./routes/blockchain');
+class BlockAPI {
 
-var app = express();
+    /**
+     * Constructor that allows us to initialize the class 
+     */
+    constructor() {
+        this.app = express();
+        this.initExpress();
+        this.initExpressMiddleWare();
+        this.initControllers();
+        this.start();
+    }
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+    /**
+     * Initialization of the Express framework
+     */
+    initExpress() {
+        this.app.set("port", 8000);
+    }
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    /**
+     * Initialization of the middleware modules
+     */
+    initExpressMiddleWare() {
+        this.app.use(bodyParser.urlencoded({extended:true}));
+        this.app.use(bodyParser.json());
+    }
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/blockchain', blockchainRouter);
+    /**
+     * Initialization of all the controllers
+     */
+    initControllers() {
+        require("./controlleres/BlockController.js")(this.app);
+    }
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+    /**
+     * Starting the REST Api application
+     */
+    start() {
+        let self = this;
+        this.app.listen(this.app.get("port"), () => {
+            console.log(`Server Listening for port: ${self.app.get("port")}`);
+        });
+    }
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+new BlockAPI();
 
-module.exports = app;
+//module.exports = app;
---
+++ controlleres/BlockController.js
@@ -0,0 +1,61 @@
+const SHA256 = require('crypto-js/sha256');
+const BlockClass = require('../models/Block.js');
+
+/**
+ * Controller Definition to encapsulate routes to work with blocks
+ */
+class BlockController {
+
+    /**
+     * Constructor to create a new BlockController, you need to initialize all your endpoints here
+     * @param {*} app 
+     */
+    constructor(app) {
+        this.app = app;
+        this.blocks = [];
+        this.initializeMockData();
+        this.getBlockByIndex();
+        this.postNewBlock();
+    }
+
+    /**
+     * Implement a GET Endpoint to retrieve a block by index, url: "/api/block/:index"
+     */
+    getBlockByIndex() {
+        this.app.get("/api/block/:index", (request, resposne) => {
+            if(this.blocks){
+                resposne.send(this.blocks[0] );
+            }
+        });
+    }
+
+    /**
+     * Implement a POST Endpoint to add a new Block, url: "/api/block"
+     */
+    postNewBlock() {
+        this.app.post("/api/block", (req, res) => {
+            // Add your code here
+        });
+    }
+
+    /**
+     * Helper method to initialize a Mock dataset. It adds 10 test blocks to the blocks array.
+     */
+    initializeMockData() {
+        if(this.blocks.length === 0){
+            for (let index = 0; index < 10; index++) {
+                let blockAux = new BlockClass.Block(`Test Data #${index}`);
+                blockAux.height = index;
+                blockAux.hash = SHA256(JSON.stringify(blockAux)).toString();
+                this.blocks.push(blockAux);
+            }
+        }
+    }
+
+}
+
+/**
+ * Exporting the BlockController class
+ * @param {*} app 
+ */
+module.exports = (app) => { return new BlockController(app);}
